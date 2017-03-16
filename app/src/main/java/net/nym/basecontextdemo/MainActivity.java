c3a3fde9d1222e698e4fd263f44605d25ae9b10e
niyueming <niyueming@163.com>
2017-03-16 11:04:58 +0800

    完成换肤，添加换肤例子

--- app/src/main/java/net/nym/basecontextdemo/MainActivity.java
+++ app/src/main/java/net/nym/basecontextdemo/MainActivity.java
@@ -2,12 +2,49 @@ package net.nym.basecontextdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+
+import com.zhy.changeskin.SkinManager;
+
+import net.nym.changeskinimpllibrary.impl.SkinManagerImpl;
+import net.nym.changeskinlibrary.operation.NSkinManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    NSkinManager<SkinManager> mSkinManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //初始化应该在application里
+        mSkinManager = SkinManagerImpl.getInstance(this);
+        mSkinManager.getManager().init(this);
+
+        mSkinManager.register(this);
+    }
+
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.button:
+                mSkinManager.changeSkin("red",null);
+                break;
+            case R.id.button2:
+                mSkinManager.changeSkin("green",null);
+                break;
+            case R.id.button3:
+                mSkinManager.clear();
+                break;
+            case R.id.button4:
+                mSkinManager.switchSkinMode(null);
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSkinManager.unregister(this);
     }
 }
